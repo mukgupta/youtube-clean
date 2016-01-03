543c04719456dc2f03a3fbef81c235dd22579ebc
Mukesh Gupta <mukeshgupta.2006@gmail.com>
2016-01-03 13:40:48 +0530

    first cut, removing honey singh and badshah from my youtube feed

---
+++ script.js
@@ -0,0 +1,24 @@
+var restrict =
+[
+"Honey Singh",
+"Badshah"
+];
+
+debugger;
+var base_selector = "ul li";
+var restrict_str = "";
+
+for (var i=0; i<restrict.length; i++) {
+	var word = restrict[i];
+	restrict_str = restrict_str + base_selector + ':contains("' + word + '")';
+	if(i!==restrict.length-1){
+		restrict_str = restrict_str + ","
+	}
+}
+
+$(function() {
+	$(restrict_str).each(function(i)
+	{
+	   $(this).remove(); // This is your rel value
+	});
+});
